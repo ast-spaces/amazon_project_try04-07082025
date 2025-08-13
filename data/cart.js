867da7d8fc4784f380d25487a8a0bdf0da754bf8
ast-space <zast_bing@yahoo.com>
2025-08-13 16:17:22 +0800

    deplicating data with product.id and cartItem.id

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,21 @@
 //create a cart array to take up the selected item => cartItem
-export let cart = [];
+export let cart = 
+[
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+  }, 
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+  }
+];
 
 export function addToCart(productId) {
   let matchingItem;
 
   cart.forEach((cartItem) => {
-    if (productId === cartItem.productId) {
+    if (productId === cartItem.id) {
       matchingItem = cartItem;
     }
   });
@@ -18,7 +28,7 @@ export function addToCart(productId) {
     matchingItem.quantity += 1;
     } else {
     cart.push({
-      productId: productId,
+      id: productId,
       quantity: 1
     });
   }
