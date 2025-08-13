f02ef2c97d50b80d4d4b159b1bc500fbd1aa75b2
ast-space <zast_bing@yahoo.com>
2025-08-13 14:29:11 +0800

    create formatCurrent() & I/E for priceCents format

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart,addToCart} from "../data/cart.js";
 import {products} from "../data/products.js";
+import {formatCurrency} from "../utils/formatCurrency.js"
 
 //use the products.js to generate the products webpage
  let productsHTML = "";
@@ -28,7 +29,7 @@ productsHTML +=
     </div>
 
     <div class="product-price">
-      $${(product.priceCents/100).toFixed(2)}
+      $${formatCurrency(product.priceCents)}
     </div>
 
     <div class="product-quantity-container">
